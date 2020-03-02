fc32c46da60e636cefc3d2081a1d5f8dff7de67e
fx_kirin <fx.kirin@gmail.com>
2020-03-02 14:47:23 +0900

    add new function

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 ``` python
-    @interval_decorator.interval(1)
-    def my_method(count):
-        print("Pass interval")
-        count += 1
-        return count
+import interval_decorator
+
+@interval_decorator.interval(1)
+def my_method(count):
+    print("Pass interval")
+    count += 1
+    return count
+
+
+@interval_decorator.interval_or_return_first_value(1)
+def my_method_or_return_first_value(count):
+    print("Pass interval")
+    updated_count = count + 1
+    return updated_count
+
+
+assert my_method(10) == 11
+assert my_method(10) == None
+assert my_method_or_return_first_value(12) == 13
+assert my_method_or_return_first_value(12) == 12
 ```
--- interval_decorator/__init__.py
+++ interval_decorator/__init__.py
@@ -1,8 +1,8 @@
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 import time
 
 
-def interval(time_interval,):
+def interval(time_interval, alternative_func=None):
     def decorator(function):
         function.__last_run = 0
 
@@ -11,8 +11,27 @@ def interval(time_interval,):
             if now - function.__last_run >= time_interval:
                 function.__last_run = now
                 return function(*args, **kwargs)
+            else:
+                if alternative_func is not None:
+                    return alternative_func(*args, **kwargs)
 
         return guard
 
     return decorator
 
+
+def interval_or_return_first_value(time_interval):
+    def decorator(function):
+        function.__last_run = 0
+
+        def guard(*args, **kwargs):
+            now = time.time()
+            if now - function.__last_run >= time_interval:
+                function.__last_run = now
+                return function(*args, **kwargs)
+            else:
+                return args[0]
+
+        return guard
+
+    return decorator
